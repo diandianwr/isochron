1a3b289708c8147d19d86e20defca97fdcfeaeec
biubiu233 <weiding@uw.edu>
2016-03-11 11:06:54 -0800

    traveldistance_peakhour

--- distance5.R
+++ distance5.R
@@ -1,5 +1,24 @@
 setwd("/Users/Wei/Documents/2016 Winter/Github/isochron")
-dist5 <- read.csv("trip_5.csv")
-test <- read.csv("trip_5_10_dis.csv")
-dist5$time_start <- strptime(dist5$time_start,format="%m/%d/%Y %H:%M:%OS")
+dist <- read.csv("trip_5_10car.csv")
+
+
+dist$time_start <- strptime(dist$time_start.x,format="%m/%d/%Y %H:%M:%OS")
+dist$group <- paste0(substr(dist$time_start,12,13))
+dist$group <- as.numeric(dist$group)
+
+for(i in 1:length(dist$group)){
+  if((dist$group[i] >=7 &&  dist$group[i] <9)){
+    dist$hour[i] <- "peakhour"
+  } else if((dist$group[i] >=16 && dist$group[i] <19)){
+    dist$hour[i] <- "peakhour"
+  }  else{
+    dist$hour[i] <- "other"
+  }
+}
+
+lunch <- subset(dist, dist$hour == "lunchhour")
+other <- subset(dist, dist$hour == "other")
+
+hist(peak$dis10)
+hist(other$dis10)
 
